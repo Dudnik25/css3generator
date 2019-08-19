1ebaa4ab20442b5dfe0dc446549613503f4780b5
Andrei Dudnik <andreidudnik6@gmail.com>
2019-08-20 00:10:26 +0300

    Add main menu

--- app/index.php
+++ app/index.php
@@ -3,19 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="CSS3 Generator by Andrei Dudnik" />
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css//libs/libs.min.css" rel="stylesheet" type="text/css">
     <link href="css/main.min.css" rel="stylesheet" type="text/css">
     <link rel="shortcut icon" href="image/favicon.png" type="image/x-icon">
     <title>CSS3 | Generator</title>
 </head>
 <body>
+<div class="wrapper">
     <header>
         <h1>CSS3 Generator</h1>
         <div class="header">
             <p>Генератор css3 стилей</p>
+            <div id="menuButton" class="clearfix">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
     </header>
+    <section id="navmenu">
+        <nav>
+            <ul class="clearfix">
+                <li><button id="BorderRadius">Border Radius</button></li>
+                <li><button id="BoxShadow">Box Shadow</button></li>
+                <li><button id="TextShadow">Text Shadow</button></li>
+                <li><button id="RGBA">RGBA</button></li>
+                <li><button id="FontFace">@Font Face</button></li>
+                <li><button id="MultipleColumn">Multiple Column</button></li>
+                <li><button id="BoxResize">Box Resize</button></li>
+                <li><button id="BoxSizing">Box Sizing</button></li>
+                <li><button id="Outline">Outline</button></li>
+                <li><button id="Transition">Transition</button></li>
+                <li><button id="Transform">Transform</button></li>
+                <li><button id="Flexbox">Flexbox</button></li>
+            </ul>
+        </nav>
+    </section>
+</div>
+
 
 
 </body>
